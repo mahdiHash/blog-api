54bd7c0d50a1fb0874c679140a0712b29657f0ec
mahdiHash <mahdihashemiii939@gmail.com>
2022-08-11 21:58:26 +0430

    Remove a logging statement

--- controllers/posts/doesExist.js
+++ controllers/posts/doesExist.js
@@ -4,7 +4,6 @@ const { BadGatewayErr } = require('../../lib/errors');
 
 // this controller checks for a post with the given title
 const controller = (req, res, next) => {
-  console.log(req.params.title);
   Post.findOne({ url_form_of_title: req.params.title.toLowerCase() })
     .exec((err, result) => {
       if (err) {
